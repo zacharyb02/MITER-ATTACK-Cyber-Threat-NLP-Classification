4d52b5b81a767bb0a4b2fcdca0d0b663c064efe6
zacharyb02 <zakariabaou2002@gmail.com>
2024-06-02 18:19:52 +0100

    processing training and deployment

---
+++ Deployment/.ipynb_checkpoints/deploy-checkpoint.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+import pandas as pd
+import joblib
+
+app= Flask(__name__)
+
+model = joblib.load(r'G:\NLP Projects\MitRA aTTACK\Notre projet\Models\ML Models\SVC.pkl')
+
+vectorizer = joblib.load(r'G:\NLP Projects\MitRA aTTACK\Notre projet\Models\ML Models\vectorizer.joblib')
+    
+FILE_PATH= 'index.html'
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template(FILE_PATH)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    inputText = request['inputText']
+    text = [inputText]
+    X = vectorizer.transform(text)
+    predictions = model.predict(X)
+    return render_template(FILE_PATH, prediction=predictions)
+
+if __name__ == "__main__":
+    app.run(port=80, debug=False)
